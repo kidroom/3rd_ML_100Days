a45d64b7ed9c7c8a1d4dabd5b7143230246a11db
kidroom <kidroom@users.noreply.github.com>
2019-09-03 23:48:18 +0800

    day6

--- homework/Day_006_HW.py
+++ homework/Day_006_HW.py
@@ -2,13 +2,19 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 dir_data = 'E:/data/'
 f_app_train = os.path.join(dir_data, 'application_train.csv')
 app_train = pd.read_csv(f_app_train)
 
+app_train = pd.read_csv(f_app_train)
 sub_train = pd.DataFrame(app_train['WEEKDAY_APPR_PROCESS_START'])
 
-lblencode = LabelEncoder()
-sub_train = lblencode.fit_transform(sub_train[:, 0])
-print(sub_train.shape)
-print(sub_train)
+onehotcode = pd.get_dummies(sub_train)
+print('before : ' + str(sub_train.shape))
+print('after : ' + str(onehotcode.shape))
+print('before : ' + str(sub_train.head()))
+print('after : ' + str(onehotcode.head()))
+
+
