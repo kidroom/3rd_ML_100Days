f5980c33f73844e2a5d26327ac6a9ff181ddd521
kidroom <kidroom@users.noreply.github.com>
2019-09-08 23:41:27 +0800

    day10

---
+++ homework/day10.py
@@ -0,0 +1,58 @@
+# 做完特徵工程前的所有準備 (與前範例相同)
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+
+data_path = 'E:/data/'
+df_train = pd.read_csv(data_path + 'house_train.csv.gz')
+
+train_Y = np.log1p(df_train['SalePrice'])
+df = df_train.drop(['Id', 'SalePrice'] , axis=1)
+df.head()
+
+#只取 int64, float64 兩種數值型欄位, 存於 num_features 中
+num_features = []
+for dtype, feature in zip(df.dtypes, df.columns):
+    if dtype == 'float64' or dtype == 'int64':
+        num_features.append(feature)
+print(f'{len(num_features)} Numeric Features : {num_features}\n')
+
+# 削減文字型欄位, 只剩數值型欄位
+df = df[num_features]
+df = df.fillna(-1)
+MMEncoder = MinMaxScaler()
+train_num = train_Y.shape[0]
+df.head()
+
+# 顯示 1stFlrSF 與目標值的散佈圖
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.regplot(x = df['1stFlrSF'][:train_num], y=train_Y)
+plt.show()
+
+# 做線性迴歸, 觀察分數
+train_X = MMEncoder.fit_transform(df)
+estimator = LinearRegression()
+cross_val_score(estimator, train_X, train_Y, cv=5).mean()
+
+# 將 1stFlrSF 限制在你覺得適合的範圍內, 調整離群值
+"""
+Your Code Here
+"""
+
+# 做線性迴歸, 觀察分數
+train_X = MMEncoder.fit_transform(df)
+estimator = LinearRegression()
+cross_val_score(estimator, train_X, train_Y, cv=5).mean()
+
+# 將 1stFlrSF 限制在你覺得適合的範圍內, 捨棄離群值
+"""
+Your Code Here
+"""
+
+# 做線性迴歸, 觀察分數
+train_X = MMEncoder.fit_transform(df)
+estimator = LinearRegression()
+cross_val_score(estimator, train_X, train_Y, cv=5).mean()
---
+++ homework/day8.py
@@ -0,0 +1,21 @@
+# Import 需要的套件
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 設定 data_path
+dir_data = 'E:/data/'
+
+f_app_train = os.path.join(dir_data, 'application_train.csv')
+app_train = pd.read_csv(f_app_train)
+
+print(app_train.head())
+
+print(app_train[''].mean())
+print(np.std(app_train[''],ddof=1))
+
+
+plt.hist(app_train[''])
+plt.show()
+
