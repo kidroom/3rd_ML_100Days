266f9ef430839fe8bc0bd10974ef074fa760a16a
kidroom <kidroom@users.noreply.github.com>
2019-09-19 09:00:07 +0800

    test

---
+++ homework/day20_wk.py
@@ -0,0 +1,22 @@
+# 載入需要的套件
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns # 另一個繪圖-樣式套件
+plt.style.use('ggplot')
+
+# 忽略警告訊息
+import warnings
+warnings.filterwarnings('ignore')
+
+x = np.random.randint(-1, 1, 1000)
+y = np.random.randint(-1, 1, 1000)
+
+matrix = 
+plt.figure(figsize=(10,10))
+"""
+Your Code Here
+"""
+heatmap = 
+plt.show()
